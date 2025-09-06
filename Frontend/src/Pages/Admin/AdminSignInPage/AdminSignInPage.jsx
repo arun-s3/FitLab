16de2eb993068c3864a120eba9bfa4bfc5ceef62
arun-s3 <arunsudhakaran01@gmail.com>
2025-09-06 17:22:41 +0530

    Made AdminSignInPage Responsive for all views; Refactored the code for AdminSignInPage

--- Frontend/src/Pages/Admin/AdminSignInPage/AdminSignInPage.jsx
+++ Frontend/src/Pages/Admin/AdminSignInPage/AdminSignInPage.jsx
@@ -1,16 +1,17 @@
 import React,{useState, useEffect} from 'react'
 import './AdminSignInPage.css'
-
 import {Link,useNavigate} from 'react-router-dom'
-import Logo from '../../../Components/Logo/Logo'
 import {useSelector,useDispatch} from 'react-redux'
-import {resetStates, adminSignin} from '../../../Slices/adminSlice'
 
 import {toast} from 'react-toastify'
 import {RiAdminLine} from "react-icons/ri";
 
 import {SiteButtonSquare} from '../../../Components/SiteButtons/SiteButtons'
 import {CustomHashLoader} from '../../../Components/Loader/Loader'
+import Logo from '../../../Components/Logo/Logo'
+import {resetStates, adminSignin} from '../../../Slices/adminSlice'
+
+
 
 export default function AdminSignInPage(){
 
@@ -112,37 +113,64 @@ export default function AdminSignInPage(){
     }
     
     return(
-            <section style={bgImg} className='h-[130vh]' id="admin-signin">
+            <section style={bgImg} 
+                className='h-[130vh]'
+                id="admin-signin">
+
                 <main className='transform translate-x-[-50%] translate-y-[-50%] absolute top-[50%] left-[50%] my-[2%] flex gap-[2rem] 
                                     items-center'>
-                    <div>
+                    <div className='hidden x-lg:inline-block'>
                         {/* <img src="/Logo_main.png" alt="Fitlab"/> */}
                         <Logo customStyle={{height:'30rem', width:'30rem'}}/>
                     </div>
-                    <div className='rounded-[22px] px-[50px] admin-form'>
+                    <div className='rounded-[22px] px-[50px] admin-form w-[24rem] s-sm:w-[29rem] sm:w-[31.5rem]'>
                         <div className='flex gap-[1rem] items-center'>
                             <RiAdminLine className='text-[40px] text-secondary'/>
                             <div className='mb-[50px] my-[50px]'>
-                                <h1 className='text-secondary font-funCity text-[27px] text-left uppercase'>ADMIN LOGIN</h1>
+                                <h1 className='text-secondary font-funCity text-[21px] s-sm:text-[27px] text-left uppercase'> ADMIN LOGIN </h1>
                             </div>
                         </div>
-                       <form className='flex flex-col gap-[15px] text-descReg1 items-start' onSubmit={(e)=>submitData(e)} >
-                            <div>
-                                <label htmlFor='identifier'>Enter your username or email address</label>
-                                <input type="text" placeholder="Username or email address" id="identifier" className='w-[31.5rem]' 
-                                        autoComplete="off" onChange={(e)=>handleChange(e)} onBlur={(e)=>{handleInput(e)}} />
+
+                       <form className='flex flex-col gap-[15px] items-start' 
+                            onSubmit={(e)=>submitData(e)} >
+
+                            <div className='w-full'>
+                                <label htmlFor='identifier' 
+                                    className='text-[13px] s-sm:text-descReg1'>
+                                         Enter your username or email address 
+                                </label>
+                                <input type="text"
+                                    placeholder="Username or email address"
+                                    id="identifier"
+                                    className='w-full' 
+                                    autoComplete="off"
+                                    onChange={(e)=> handleChange(e)}
+                                    onBlur={(e)=> handleInput(e)} />
                                 <p className='error'></p>
                             </div>
-                            <div>
-                                <label htmlFor='password'>Enter your Password</label>
-                                <input type="password" placeholder="Password" id="password" onChange={(e)=>handleChange(e)} 
-                                                    autoComplete="off" onBlur={(e)=>{handleInput(e)}} />
+                            <div className='w-full'>
+                                <label htmlFor='password'
+                                    className='text-[13px] s-sm:text-descReg1 '>
+                                        Enter your Password
+                                </label>
+                                <input type="password"
+                                    placeholder="Password"
+                                    id="password"
+                                    className='w-full'
+                                    onChange={(e)=> handleChange(e)} 
+                                    autoComplete="off"
+                                    onBlur={(e)=> handleInput(e)} />
                                 <p className='error' ></p>
                             </div>
-                            <Link to="" className='text-subtitleSmall1 text-secondary ml-[4px]'> Forgot Password</Link>
-                            <SiteButtonSquare shouldSubmit={true} customStyle={{width:'31.5rem', marginBottom:'60px'}} >
+                            <Link to=""
+                                className='text-[11px] s-sm:text-subtitleSmall1 text-secondary ml-[4px]'>
+                                    Forgot Password
+                            </Link>
+                            <SiteButtonSquare shouldSubmit={true} 
+                                tailwindClasses='w-full' 
+                                customStyle={{marginBottom:'60px'}} >
                                 { adminLoading? <CustomHashLoader adminLoading={adminLoading}/>: "Sign In" }
-                             </SiteButtonSquare>
+                            </SiteButtonSquare>
                         </form>
                     </div>
                     

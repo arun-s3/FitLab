8d366c9396739825f8b599f3058d066fc2033984
arun-s3 <arunsudhakaran01@gmail.com>
2026-02-01 23:52:22 +0530

    Updated instances and exec_mode of ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  apps: [
-    {
-      name: "Fitlab-backend",
-      script: "index.js",  
-      instances: "max",    
-      exec_mode: "cluster",
-      env_production: {
-        NODE_ENV: "production",
-        PORT: 3000
-      },
-      watch: false,             
-      max_memory_restart: "500M" 
-    }
-  ]
-};
+    apps: [
+        {
+            name: "Fitlab-backend",
+            script: "index.js",
+            instances: 1, 
+            exec_mode: "fork", 
+            env: {
+              NODE_ENV: "production",
+              PORT: 3000
+            },
+            watch: false,
+            max_memory_restart: "500M",
+        },
+    ],
+}
